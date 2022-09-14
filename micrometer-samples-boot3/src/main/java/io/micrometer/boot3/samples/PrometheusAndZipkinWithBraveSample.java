e77b2850f52cb1d60a50c417567e23227d73e869
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2022-09-13 17:16:29 -0700

    Add prometheus and grafana

--- micrometer-samples-boot3/src/main/java/io/micrometer/boot3/samples/PrometheusAndZipkinWithBraveSample.java
+++ micrometer-samples-boot3/src/main/java/io/micrometer/boot3/samples/PrometheusAndZipkinWithBraveSample.java
@@ -15,14 +15,102 @@
  */
 package io.micrometer.boot3.samples;
 
+import io.micrometer.core.instrument.Meter;
+import io.micrometer.core.instrument.config.MeterFilter;
+import io.micrometer.core.instrument.distribution.DistributionStatisticConfig;
+import io.micrometer.tracing.Tracer;
+import io.prometheus.client.exemplars.tracer.common.SpanContextSupplier;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.SmartInitializingSingleton;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PrometheusAndZipkinWithBraveSample {
 
     public static void main(String[] args) {
         SpringApplication.run(PrometheusAndZipkinWithBraveSample.class, args);
+    }
+
+    @Bean
+    MeterFilter meterFilter() {
+        return new MeterFilter() {
+            @Override
+            public DistributionStatisticConfig configure(Meter.Id id, DistributionStatisticConfig config) {
+                if (id.getName().equals("greeting")) {
+                    return DistributionStatisticConfig.builder().percentilesHistogram(true).build().merge(config);
+                }
+                else {
+                    return config;
+                }
+            }
+        };
+    }
+
+    @Bean
+    SpanContextSupplier tracingSpanContextSupplier(ObjectProvider<Tracer> tracerProvider) {
+        return new LazyTracingSpanContextSupplier(tracerProvider);
+    }
+
+    static class LazyTracingSpanContextSupplier implements SpanContextSupplier, SmartInitializingSingleton {
+
+        private final ObjectProvider<Tracer> tracerProvider;
+
+        private SpanContextSupplier delegate;
+
+        LazyTracingSpanContextSupplier(ObjectProvider<Tracer> tracerProvider) {
+            this.tracerProvider = tracerProvider;
+        }
+
+        @Override
+        public String getTraceId() {
+            return this.delegate.getTraceId();
+        }
+
+        @Override
+        public String getSpanId() {
+            return this.delegate.getSpanId();
+        }
+
+        @Override
+        public boolean isSampled() {
+            return this.delegate != null && this.delegate.isSampled();
+        }
+
+        @Override
+        public void afterSingletonsInstantiated() {
+            Tracer tracer = tracerProvider.getIfAvailable();
+            if (tracer != null) {
+                this.delegate = new TracingSpanContextSupplier(tracer);
+            }
+        }
+
+    }
+
+    static class TracingSpanContextSupplier implements SpanContextSupplier {
+
+        private final Tracer tracer;
+
+        TracingSpanContextSupplier(Tracer tracer) {
+            this.tracer = tracer;
+        }
+
+        @Override
+        public String getTraceId() {
+            return tracer.currentSpan().context().traceId();
+        }
+
+        @Override
+        public String getSpanId() {
+            return tracer.currentSpan().context().spanId();
+        }
+
+        @Override
+        public boolean isSampled() {
+            return tracer.currentSpan() != null ? tracer.currentSpan().context().sampled() : false;
+        }
+
     }
 
 }
--- micrometer-samples-boot3/src/main/java/io/micrometer/boot3/samples/controller/SampleController.java
+++ micrometer-samples-boot3/src/main/java/io/micrometer/boot3/samples/controller/SampleController.java
@@ -15,34 +15,80 @@
  */
 package io.micrometer.boot3.samples.controller;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
 import io.micrometer.observation.Observation;
 import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.tracing.Tracer;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 class SampleController {
 
+    private final List<String> PEOPLE = Arrays.asList("suzy", "mike");
+
     private final ObservationRegistry registry;
 
-    SampleController(ObservationRegistry registry) {
+    private final Tracer tracer;
+
+    SampleController(ObservationRegistry registry, Tracer tracer) {
         this.registry = registry;
+        this.tracer = tracer;
     }
 
     @GetMapping("/")
-    String hello() {
-        return Observation.createNotStarted("test.observation", registry).observe(this::fetchGreeting);
+    List<String> allPeople() {
+        return Observation.createNotStarted("allPeople", registry).observe(slowDown(() -> PEOPLE));
     }
 
-    private String fetchGreeting() {
-        try {
-            Thread.sleep(((int) (Math.random() * 100)) + 100);
+    @GetMapping("/greet/{name}")
+    String greet(@PathVariable String name) {
+        Observation observation = Observation.createNotStarted("greeting", registry).start();
+        try (Observation.Scope scope = observation.openScope()) {
+            if (PEOPLE.contains(name)) {
+                // only 2 names are valid (low cardinality)
+                observation.lowCardinalityKeyValue("greeting.name", name);
+                observation.event(Observation.Event.of("greeted"));
+                return fetchDataSlowly(() -> String.format("Hello %s!", name));
+            }
+            else {
+                observation.lowCardinalityKeyValue("greeting.name", "N/A");
+                observation.highCardinalityKeyValue("greeting.name", name);
+                observation.event(Observation.Event.of("failed"));
+                throw new IllegalArgumentException("Invalid name!");
+            }
         }
-        catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        catch (Exception exception) {
+            observation.error(exception);
+            throw exception;
         }
+        finally {
+            observation.stop();
+        }
+    }
 
-        return "hello";
+    private <T> Supplier<T> slowDown(Supplier<T> supplier) {
+        return () -> {
+            try {
+                if (Math.random() < 0.1) { // larger latency, less frequent
+                    Thread.sleep(500);
+                }
+                Thread.sleep(((int) (Math.random() * 100)) + 100); // +base latency
+            }
+            catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            return supplier.get();
+        };
+    }
+
+    private <T> T fetchDataSlowly(Supplier<T> supplier) {
+        return slowDown(supplier).get();
     }
 
 }
