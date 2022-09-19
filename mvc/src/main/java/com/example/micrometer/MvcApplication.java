7485ae550d197d6c205f660a57545b037b59c554
Marcin Grzejszczak <mgrzejszczak@vmware.com>
2022-09-19 16:51:34 +0200

    Added openfeign + mvc samples

---
+++ mvc/src/main/java/com/example/micrometer/MvcApplication.java
@@ -0,0 +1,50 @@
+package com.example.micrometer;
+
+import java.util.Map;
+
+import io.micrometer.tracing.Tracer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRequest;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class MvcApplication {
+
+    public static void main(String... args) {
+        new SpringApplication(MvcApplication.class).run(args);
+    }
+}
+
+@RestController
+class MvcController {
+    private static final Logger log = LoggerFactory.getLogger(MvcController.class);
+
+    private final Tracer tracer;
+
+    MvcController(Tracer tracer) {
+        this.tracer = tracer;
+    }
+
+    // TODO: Uncomment this once Mvc gets instrumented in Framework
+//    @GetMapping("/")
+//    public String span() {
+//        String traceId = this.tracer.currentSpan().context().traceId();
+//        log.info("<ACCEPTANCE_TEST> <TRACE:{}> Hello from producer", traceId);
+//        return traceId;
+//    }
+
+    @GetMapping("/")
+    public String span(@RequestHeader Map<String,String> headers) {
+        String traceId = headers.get("traceparent");
+        Assert.notNull(traceId, "traceparent must not be null");
+        log.info("<ACCEPTANCE_TEST> <TRACE:{}> Hello from producer", traceId.split("-")[1]);
+        return traceId;
+    }
+}
---
+++ openfeign/src/main/java/com/example/micrometer/OpenFeignApplication.java
@@ -0,0 +1,63 @@
+package com.example.micrometer;
+
+import io.micrometer.tracing.Span;
+import io.micrometer.tracing.Tracer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@EnableFeignClients
+public class OpenFeignApplication implements CommandLineRunner {
+
+    public static void main(String... args) {
+        new SpringApplicationBuilder(OpenFeignApplication.class).web(WebApplicationType.NONE).run(args);
+    }
+
+    @Autowired
+    OpenFeignService openFeignService;
+
+    @Override
+    public void run(String... args) throws Exception {
+        this.openFeignService.call();
+    }
+}
+
+@Service
+class OpenFeignService {
+    private static final Logger log = LoggerFactory.getLogger(OpenFeignService.class);
+
+    private final ProducerClient producerClient;
+
+    private final Tracer tracer;
+
+    OpenFeignService(ProducerClient producerClient, Tracer tracer) {
+        this.producerClient = producerClient;
+        this.tracer = tracer;
+    }
+
+    String call() {
+        Span span = this.tracer.nextSpan();
+        try (Tracer.SpanInScope ws = this.tracer.withSpan(span.start())) {
+            log.info("<ACCEPTANCE_TEST> <TRACE:{}> Hello from consumer", this.tracer.currentSpan().context().traceId());
+            return this.producerClient.callProducer();
+        }
+        finally {
+            span.end();
+        }
+    }
+}
+
+@FeignClient(url = "${url:http://localhost:7100}", name = "producer")
+interface ProducerClient {
+    @GetMapping
+    String callProducer();
+}
