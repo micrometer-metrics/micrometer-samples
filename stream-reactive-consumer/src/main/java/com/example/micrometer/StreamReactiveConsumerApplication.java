b44005562cb8c0a4836c0b9095adc5f2c14503dc
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2022-11-01 17:54:40 -0700

    Fix rabbit reactive test? It seems the instrumentation already propagates the context so we don't need to do anything special

--- stream-reactive-consumer/src/main/java/com/example/micrometer/StreamReactiveConsumerApplication.java
+++ stream-reactive-consumer/src/main/java/com/example/micrometer/StreamReactiveConsumerApplication.java
@@ -1,24 +1,18 @@
 package com.example.micrometer;
 
-import java.util.function.Consumer;
-
-import io.micrometer.context.ContextSnapshot;
-import io.micrometer.observation.Observation;
 import io.micrometer.observation.ObservationRegistry;
-import io.micrometer.observation.contextpropagation.ObservationThreadLocalAccessor;
 import io.micrometer.tracing.Tracer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-import reactor.util.context.ContextView;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.Message;
+import reactor.core.publisher.Flux;
+
+import java.util.function.Consumer;
 
 @SpringBootApplication
 public class StreamReactiveConsumerApplication implements CommandLineRunner {
@@ -37,46 +31,8 @@ public class StreamReactiveConsumerApplication implements CommandLineRunner {
 
     @Bean
     Consumer<Flux<Message<String>>> channel(Tracer tracer, ObservationRegistry observationRegistry) {
-        // For the reactive consumer remember to call "subscribe()" at the end, otherwise
-        // you'll get the "Dispatcher has no subscribers" error
-        Observation observation = Observation.start("stream-reactive-consumer-app", observationRegistry);
-
-        Consumer<Flux<Message<String>>> result = messageFlux -> {
-            messageFlux.flatMap(message -> Mono.deferContextual(contextView -> {
-                Runnable runnable = () -> {
-                    log.info("<ACCEPTANCE_TEST> <TRACE:{}> Hello from consumer",
-                            tracer.currentSpan().context().traceId());
-                };
-                scoped(contextView, runnable);
-                return Mono.just(message);
-
-            })).doOnNext(s -> {
-                // tracer.currentSpan().end();
-                // tracer.withSpan(null);
-            }).doFinally(signalType -> observation.stop())
-                    .contextWrite(context -> context.put(ObservationThreadLocalAccessor.KEY, observation)).subscribe();
-        };
-        return result;
-
-// @formatter:off
-//		return i -> i
-//					.doOnNext(s ->
-//						log.info("<ACCEPTANCE_TEST> <TRACE:{}> Hello from consumer", tracer.currentSpan().context().traceId()))
-//					// You must finish the span yourself and clear the tracing context like presented below.
-//					// Otherwise you will be missing out the span that wraps the function execution.
-//					.doOnNext(s -> {
-//						tracer.currentSpan().end();
-//						tracer.withSpan(null);
-//					})
-//					.subscribe();
-// @formatter:on
-    }
-
-    private void scoped(ContextView contextView, Runnable runnable) {
-        try (ContextSnapshot.Scope scope = ContextSnapshot.setThreadLocalsFrom(contextView,
-                ObservationThreadLocalAccessor.KEY)) {
-            runnable.run();
-        }
+        return flux -> flux.doOnNext(msg -> log.info("<ACCEPTANCE_TEST> <TRACE:{}> Hello from consumer",
+                tracer.currentSpan().context().traceId())).subscribe();
     }
 
 }
