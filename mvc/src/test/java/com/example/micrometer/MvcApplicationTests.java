c1c1852b035f57384da974396d57fba951804b64
Tadaya Tsuyukubo <tadaya@ttddyy.net>
2022-11-02 07:55:12 -0700

    Fix test compilation

--- mvc/src/test/java/com/example/micrometer/MvcApplicationTests.java
+++ mvc/src/test/java/com/example/micrometer/MvcApplicationTests.java
@@ -4,9 +4,11 @@ import io.micrometer.core.instrument.MeterRegistry;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.actuate.observability.AutoConfigureObservability;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 @AutoConfigureObservability
 class MvcApplicationTests {
 
@@ -15,9 +17,7 @@ class MvcApplicationTests {
 
     @Test
     void should_work_for_metrics() {
-        this.meterRegistry.getMeters()
+        this.meterRegistry.getMeters();
     }
-
-    static class
 
 }
