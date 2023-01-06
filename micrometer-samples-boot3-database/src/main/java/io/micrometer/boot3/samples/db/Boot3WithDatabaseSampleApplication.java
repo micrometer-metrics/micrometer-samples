c22f744a3ca79e152eb90771a745de3b5885d50d
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2023-01-05 17:25:28 -0800

    Add AOT capabilities to the Boot3 samples (#5)
    * Add AOT capabilities to the Boot3 samples
    
    * Disable format/checkstyle for aot generated sourceSet
    
    * Generalize graalvmNative config
    
    * Fix Boot native bits

--- micrometer-samples-boot3-database/src/main/java/io/micrometer/boot3/samples/db/Boot3WithDatabaseSampleApplication.java
+++ micrometer-samples-boot3-database/src/main/java/io/micrometer/boot3/samples/db/Boot3WithDatabaseSampleApplication.java
@@ -15,14 +15,29 @@
  */
 package io.micrometer.boot3.samples.db;
 
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportRuntimeHints;
+import org.springframework.core.io.ClassPathResource;
 
 @SpringBootApplication
+@ImportRuntimeHints(Boot3WithDatabaseSampleApplication.ResourceHints.class)
 public class Boot3WithDatabaseSampleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Boot3WithDatabaseSampleApplication.class, args);
+    }
+
+    static class ResourceHints implements RuntimeHintsRegistrar {
+
+        @Override
+        public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+            hints.resources().registerResource(new ClassPathResource("itest-data.sql"));
+            hints.resources().registerResource(new ClassPathResource("itest-schema.sql"));
+        }
+
     }
 
 }
