d32d372cbf282e54edabb1360ae87b8cbc673811
nathandeamer <nd@nathandeamer.com>
2023-10-14 10:44:29 +0100

    Created retrofit sample

---
+++ retrofit/src/test/java/com/example/micrometer/RetrofitApplicationTests.java
@@ -0,0 +1,59 @@
+package com.example.micrometer;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.tck.MeterRegistryAssert;
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.tracing.Tracer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.actuate.observability.AutoConfigureObservability;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+
+@AutoConfigureObservability
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class RetrofitApplicationTests {
+
+    static WireMockServer wireMockServer = new WireMockServer(wireMockConfig().port(7100));
+
+    @LocalServerPort
+    int port;
+
+    @Autowired
+    MeterRegistry meterRegistry;
+
+    @Autowired
+    ObservationRegistry registry;
+
+    @Autowired
+    Tracer tracer;
+
+    @BeforeAll
+    static void setup() {
+        wireMockServer.start();
+        wireMockServer.stubFor(WireMock.get("/").willReturn(WireMock.aResponse().withBody("foo").withStatus(200)));
+    }
+
+    @AfterAll
+    static void close() {
+        wireMockServer.stop();
+    }
+
+    @Test
+    void should_record_metrics() {
+        MeterRegistryAssert.then(meterRegistry)
+            .hasMeterWithNameAndTagKeys("okhttp", "error", "host", "method", "outcome", "status", "target.host",
+                    "target.port", "target.scheme", "uri");
+
+        MeterRegistryAssert.then(meterRegistry)
+            .hasMeterWithNameAndTagKeys("okhttp.active", "host", "method", "outcome", "status", "target.host",
+                    "target.port", "target.scheme", "uri");
+    }
+
+}
